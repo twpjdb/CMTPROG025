f9ae8594b62291537bc2379e7e872cbf0ee3513a
twpjdb <44226640+twpjdb@users.noreply.github.com>
2019-10-15 14:38:21 +0200

    Created user model for login

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function recipes()
+    {
+        return $this->hasMany('App\Recipes');
+    }
 }
