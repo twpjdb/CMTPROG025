1e9a172b5fcbd4ae231e32c90f39df18c3144418
twpjdb <44226640+twpjdb@users.noreply.github.com>
2019-10-15 14:37:12 +0200

    Created migrations for models

---
+++ database/migrations/2019_10_15_113354_create_recipes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('category_id');
+            $table->timestamps();
+        });
+
+        // Create pivot table for the many to many relationship.
+        Schema::create('ingredient_recipe', function (Blueprint $table) {
+            $table->unsignedInteger('recipe_id');
+            $table->unsignedInteger('ingredient_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+        Schema::dropIfExists('ingredient_recipe');
+    }
+}
